e7acbf2776604f0e1df0c6bb9cc5b52fbfa335fc
janda96@github.com <janda96@github.com>
2019-01-22 16:26:54 -0500

    Added odometry and basic moving code

--- src/mie443_contest1/src/contest1.cpp
+++ src/mie443_contest1/src/contest1.cpp
@@ -5,16 +5,21 @@
 #include <sensor_msgs/LaserScan.h>
 #include <eStop.h>
 
+#include <nav_msgs/Odometry.h>
+#include <tf/transform_datatypes.h>
+
 #include <stdio.h>
 #include <cmath>
 #include <ctime>
 
 using namespace std;
 
-double angular = 0.0;
-double linear = 0.0;
+// CALLBACK VARIABLES
+bool bumperLeft = false, bumperCenter = false, bumperRight = false;
 
-double BUMPER_STATE = 0;
+// Odometry
+double posX, posY, yaw;
+double pi = 3.1416;
 
 void delaySec(double seconds)
 {
@@ -33,12 +38,37 @@ void delaySec(double seconds)
 
 void bumperCallback(const kobuki_msgs::BumperEvent::ConstPtr& msg)
 {
-	BUMPER_STATE = msg->state;
+	if (msg->bumper == 0)
+	{
+		bumperLeft = !bumperLeft;
+	}
+	else if (msg->bumper == 1)
+	{
+		bumperCenter = !bumperCenter;
+	}
+	else if (msg->bumper == 2)
+	{
+		bumperRight = !bumperRight;
+	}
 }
 
 void laserCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
 {
 	// fill me in
+}
+
+void odomCallback(const nav_msgs::Odometry::ConstPtr& msg)
+{
+	posX = msg->pose.pose.position.x;
+	posY = msg->pose.pose.position.y;
+	yaw = tf::getYaw(msg->pose.pose.orientation);
+
+	ROS_INFO("Position: (%f, %f) Orientation: %f rad or %f degrees.", posX, posY, yaw, yaw*180.0/pi);
+}
+
+bool isBumperPressed()
+{
+	return bumperRight || bumperCenter || bumperLeft;
 }
 
 int main(int argc, char **argv)
@@ -47,33 +77,47 @@ int main(int argc, char **argv)
 	ros::NodeHandle nh;
 	teleController eStop;
 
+	// Subscritpions
 	ros::Subscriber bumper_sub = nh.subscribe("mobile_base/events/bumper", 10, &bumperCallback);
 	ros::Subscriber laser_sub = nh.subscribe("scan", 10, &laserCallback);
+	ros::Subscriber odom = nh.subscribe("/odom", 1, odomCallback);
 
+	// Publishers
 	ros::Publisher vel_pub = nh.advertise<geometry_msgs::Twist>("cmd_vel_mux/input/teleop", 1);
 
 	geometry_msgs::Twist vel;
 	
-	linear = 0.2;
-	//angular = 0.5;
-	while(ros::ok()){
-		ros::spinOnce();	// UPDATE VALUES IN CALLBACK SUBSCRIPTION
+	double linear = 0.0;
+	double angular = 0.0;
+	while(ros::ok())
+	{
+		// UPDATE VALUES IN CALLBACK SUBSCRIPTION
+		ros::spinOnce();	
+
 		//.....**E-STOP DO NOT TOUCH**.......
 		eStop.block();
 		//...................................
 
-		// fill me in
-		if (BUMPER_STATE == 1)
+		angular = 0.0;
+		linear = 0.0;
+
+		if (!isBumperPressed())
 		{
+			if (posX < 0.5 && yaw < pi/12)
+			{
+				angular = 0.0;
+				linear = 0.2;
+			}
+			else if (yaw < pi/2 && posX > 0.5)
+			{
+				angular = pi/6;
+				linear = 0.0;
+			}
+		}
+		else
+		{
+			angular = 0.0;
 			linear = 0.0;
-			angular = 0.5;
-
-			vel.angular.z = angular;
-  			vel.linear.x = linear;
-
-  			vel_pub.publish(vel);
-
-			//delaySec(0.1);
 		}
 		
 
