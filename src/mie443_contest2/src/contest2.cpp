1d0b2294b5c71748d17b255f3c5615a3f9f37d4a
janda96@github.com <janda96@github.com>
2019-03-05 18:57:41 -0500

    Added first iteration of imagae reconition code

--- src/mie443_contest2/src/contest2.cpp
+++ src/mie443_contest2/src/contest2.cpp
@@ -5,6 +5,8 @@
 #include <math.h>
 #include <cmath>
 
+#include <fstream>
+
 int main(int argc, char** argv) {
     // Setup ROS.
     ros::init(argc, argv, "contest2");
@@ -30,40 +32,53 @@ int main(int argc, char** argv) {
     }
 
     // Initialize image objectand subscriber.
-    //ImagePipeline imagePipeline(n);
+    ImagePipeline imagePipeline(n);
 
     // Initalize navigator
     Navigation navigator;
 
-    unsigned boxInd = 0;
+    // Initalize which box is currently being navigated to
+    unsigned boxInd = 0;  
+
+    std::ifstream BoxIDs("BoxIDs.txt");
+
+    // Make sure initalization completes
+    for (auto i = 0; i < 30; ++i)
+    {
+        ros::spinOnce();
+        ros::Duration(0.01).sleep();
+    }
 
     // Execute strategy.
     while(ros::ok()) 
     {
         ros::spinOnce();
-        /***YOUR CODE HERE***/
         // Use: boxes.coords
         // Use: robotPose.x, robotPose.y, robotPose.phi
 
-        float phi = boxes.coords[boxInd][2];
-        float x = boxes.coords[boxInd][0] + 0.7*std::cos(phi);
-        float y = boxes.coords[boxInd][1] + 0.7*std::sin(phi);
-        phi = fmod(phi + 3.14, 6.28);
+        // float phi = boxes.coords[boxInd][2];
+        // float x = boxes.coords[boxInd][0] + 0.7*std::cos(phi);
+        // float y = boxes.coords[boxInd][1] + 0.7*std::sin(phi);
+        // phi = fmod(phi + 3.14, 6.28);
 
-        if (!navigator.moveToGoal(x, y, phi))
-        {
-            return -1;
-        }
+        // if (!navigator.moveToGoal(x, y, phi))
+        // {
+        //     return -1;
+        // }
 
-        ++boxInd;
-        if (boxInd > static_cast<int>(boxes.coords.size()))
-        {
-            std::cout << "Found all boxes" << std::endl;
-            return 0;
-        }
+        // ++boxInd;
 
-        //simagePipeline.getTemplateID(boxes);
-        //ros::Duration(0.01).sleep();
+        auto templateID = imagePipeline.getTemplateID(boxes);
+        ROS_INFO("Got Template ID");
+        printf("Image Index: %d \n", templateID);
+        ros::Duration(0.01).sleep();
+
+        // // Check stop condition
+        // if (boxInd > static_cast<int>(boxes.coords.size()))
+        // {
+        //     std::cout << "Found all boxes" << std::endl;
+        //     return 0;
+        // }
     }
     return 0;
 }
--- src/mie443_contest2/src/imagePipeline.cpp
+++ src/mie443_contest2/src/imagePipeline.cpp
@@ -1,8 +1,16 @@
 #include <imagePipeline.h>
 
+// OPENCV
+#include "opencv2/highgui.hpp"
+#include "opencv2/calib3d.hpp"
+#include "opencv2/xfeatures2d.hpp"
+#include "opencv2/imgproc.hpp"
+
 #define IMAGE_TYPE sensor_msgs::image_encodings::BGR8
-#define IMAGE_TOPIC "camera/rgb/image_raw" // kinect:"camera/rgb/image_raw" webcam:"camera/image"
-//#define IMAGE_TOPIC "camera/image" //Kinect:"camera/rgb/image_raw" webcam:"camera/image"
+#define IMAGE_TOPIC "camera/image" // kinect:"camera/rgb/image_raw" webcam:"camera/image"
+
+using namespace cv;
+using namespace cv::xfeatures2d;
 
 ImagePipeline::ImagePipeline(ros::NodeHandle& n) {
     image_transport::ImageTransport it(n);
@@ -26,18 +34,100 @@ void ImagePipeline::imageCallback(const sensor_msgs::ImageConstPtr& msg) {
 
 int ImagePipeline::getTemplateID(Boxes& boxes) {
     int template_id = -1;
-    if(!isValid) {
+    if(!isValid) 
+    {
         std::cout << "ERROR: INVALID IMAGE!" << std::endl;
-    } else if(img.empty() || img.rows <= 0 || img.cols <= 0) {
+    }
+    else if(img.empty() || img.rows <= 0 || img.cols <= 0) 
+    {
         std::cout << "ERROR: VALID IMAGE, BUT STILL A PROBLEM EXISTS!" << std::endl;
         std::cout << "img.empty():" << img.empty() << std::endl;
         std::cout << "img.rows:" << img.rows << std::endl;
         std::cout << "img.cols:" << img.cols << std::endl;
-    } else {
+    } 
+    else 
+    {
         /***YOUR CODE HERE***/
         // Use: boxes.templates
         cv::imshow("view", img);
         cv::waitKey(10);
+
+        int minHessian = 400;
+        Ptr<SURF> detector = SURF::create(minHessian);
+        FlannBasedMatcher matcher;
+
+        std::vector<KeyPoint> keypoints_object;
+        Mat descriptors_object;
+
+        detector->detectAndCompute(img, Mat(), keypoints_object, descriptors_object);
+
+        std::vector<size_t> numMatches(boxes.templates.size(), 0);
+
+        // Go through each box and match
+        for (size_t boxInd = 0; boxInd < boxes.templates.size(); ++boxInd)
+        {
+            auto& boxTemplate = boxes.templates[boxInd];
+
+            Mat descriptors_scene;
+            std::vector<KeyPoint> keypoints_scene;
+            detector->detectAndCompute(boxTemplate, Mat(), keypoints_scene, descriptors_scene);
+            
+            // Feature Matching
+            std::vector<DMatch> matches;
+            matcher.match(descriptors_object, descriptors_scene, matches);
+
+            printf("Number of matches: %d \n", static_cast<int>(matches.size()));
+
+            double max_dist = 0.0, min_dist = 100.0;
+            for (size_t i = 0; i < matches.size(); ++i)
+            {
+                double dist = matches[i].distance;
+                min_dist = std::min(min_dist, dist);
+                max_dist = std::max(max_dist, dist);
+            }
+
+            printf("Min Dist: %lf \n", min_dist);
+            printf("Max Dist: %lf \n", max_dist);
+
+            if (min_dist > 0.25)
+            {
+                break;
+            }
+
+            unsigned numGoodMatches = 0;
+            for (size_t i = 0; i < matches.size(); ++i)
+            {
+                if (matches[i].distance < 1.5 * min_dist)
+                {
+                    ++numGoodMatches;
+                }
+            }
+
+            printf("Number of good matches: %d \n", numGoodMatches);
+            numMatches[boxInd] = numGoodMatches;
+        }
+        
+        size_t maxMatches = 0;
+        for (size_t boxInd = 0; boxInd < numMatches.size(); ++boxInd)
+        {
+            if (maxMatches < numMatches[boxInd])
+            {
+                template_id = static_cast<int>(boxInd);
+                maxMatches = numMatches[boxInd];
+            }
+        }
+
+        if (maxMatches < 10)
+        {
+            template_id = -1;
+        }
     }  
+
+    if (template_id >= 0)
+    {
+        cv::imshow("view", boxes.templates[template_id]);
+        cv::waitKey(2000);
+    }
+
     return template_id;
 }
